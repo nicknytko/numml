780db498397f906dde8362ca23752fc0b48f9d2e
Nicolas Nytko <nnytko2@illinois.edu>
2022-09-12 22:25:32 -0500

    basic csr implementation with spmv

--- numml/torch/sparse.py
+++ numml/torch/sparse.py
@@ -3,11 +3,10 @@ import torch.autograd
 import numml_torch_cpp
 import numpy as np
 
-class torchSparseCOOToNummlCSR(torch.autograd.Function):
+class coo_to_csr(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, coo_tensor):
-        row_ind, col_ind = coo_tensor.indices()
-        data = coo_tensor.values()
+    def forward(ctx, values, row_ind, col_ind, shape):
+        data = torch.clone(values).detach()
         dtype = data.dtype
         N = len(row_ind)
 
@@ -17,40 +16,70 @@ class torchSparseCOOToNummlCSR(torch.autograd.Function):
 
         # create row indices
         rowsort = row_ind[argsort]
-        rowptr = torch.zeros(coo_tensor.shape[0], dtype=dtype)
+        rowptr = torch.zeros(shape[0] + 1, dtype=dtype)
         cur_row = -1
         for i in range(N):
             if cur_row != row_ind[i]:
                 cur_row = row_ind[i]
                 rowptr[cur_row] = i
+        rowptr[-1] = len(data)
 
         ctx.save_for_backward(argsort_inv, row_ind, col_ind)
-        return SparseCSRTensor(data, col_ind[argsort], rowptr, coo_tensor.shape)
+        return data, col_ind[argsort], rowptr.long()
 
     @staticmethod
-    def backward(ctx, csr_tensor):
+    def backward(ctx, data, col_ind, rowptr):
         argsort_inv, row_ind, col_ind = ctx.saved_tensors
-        return torch.sparse_coo_tensor(torch.Tensor((row_ind, col_ind)),
-                                       csr_tensor.data[argsort_inv],
-                                       size=csr_tensor.shape)
+        return data[argsort_inv], None, None, None
 
-class nummlCSRSum(torch.autograd.Function):
+
+class csr_spmv(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, csr):
-        ctx.save_for_backward(csr)
-        return torch.sum(csr.data)
+    def forward(ctx, data, col_ind, rowptr, shape, x):
+        y = torch.zeros(shape[0], dtype=x.dtype)
+        for row_i in range(len(rowptr) - 1):
+            for col_j in range(rowptr[row_i], rowptr[row_i + 1]):
+                y[row_i] += data[col_j] * x[col_ind[col_j]]
+        return y
 
     @staticmethod
-    def backward(ctx, grad):
-        csr, = ctx.saved_tensors
-        return SparseCSRTensor(grad * torch.ones_like(csr.data), csr.indices, csr.indptr, csr.shape)
+    def backward(ctx, data, col_ind, rowptr):
+        return None, None, None, None, None
+
 
 class SparseCSRTensor(object):
-    def __init__(self, data, indices, indptr, shape):
-        self.data = data
-        self.indices = indices
-        self.indptr = indptr
-        self.shape = shape
+    def __init__(self, arg1, shape=None):
+        if isinstance(arg1, torch.Tensor):
+            if arg1.layout == torch.sparse_coo:
+                arg1 = arg1.coalesce()
+                vals = arg1.values()
+                rows, cols = arg1.indices()
+                self.data, self.indices, self.indptr = coo_to_csr.apply(vals, rows, cols, arg1.shape)
+                if shape is not None:
+                    self.shape = shape
+                else:
+                    self.shape = arg1.shape
+            else:
+                raise RuntimeError('not implemented: csr from dense')
+
+    def spmv(self, x):
+        return csr_spmv.apply(self.data, self.indices, self.indptr, self.shape, x)
+
+    def __matmul__(self, x):
+        dims = len(torch.squeeze(x).shape)
+        if dims == 1:
+            return self.spmv(x)
+        elif dims == 2:
+            raise RuntimeError('not implemented: spmm/spspmm')
+        else:
+            raise RuntimeError(f'invalid tensor found for sparse multiply: mode {dims} tensor found.')
+
+    def to_dense(self):
+        X = torch.zeros(self.shape)
+        for row_i in range(len(self.indptr) - 1):
+            for data_j in range(self.indptr[row_i], self.indptr[row_i + 1]):
+                X[row_i, self.indices[data_j]] = self.data[data_j]
+        return X
 
     def sum(self):
-        return nummlCSRSum.apply(self)
+        return self.vals.sum()
