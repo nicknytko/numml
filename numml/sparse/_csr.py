f1f26ef7f3ab1e86fe9401e7f94d520aa8eaf3c0
Nicolas Nytko <nnytko2@illinois.edu>
2023-12-06 15:52:23 -0600

    Add support for batched multiplies

--- numml/sparse/_csr.py
+++ numml/sparse/_csr.py
@@ -684,7 +684,7 @@ class SparseCSRTensor(object):
     def __matmul__(self, x):
         dims = None
         if isinstance(x, torch.Tensor):
-            dims = len(torch.squeeze(x).shape)
+            dims = x.ndim
         elif isinstance(x, SparseCSRTensor):
             dims = 2
         else:
@@ -709,7 +709,18 @@ class SparseCSRTensor(object):
             elif isinstance(x, torch.Tensor):
                 return self.spdmm(x)
         else:
-            raise RuntimeError(f'invalid tensor found for sparse multiply: mode {dims} tensor found.')
+            if isinstance(x, torch.Tensor) and x.shape[-2] == self.shape[1]:
+                # If we're multiplying by a 3d or more complex shape, assume this is a batched SPDMM
+                col_size = int(torch.prod(torch.tensor(x.shape[:-2])).item()) * x.shape[-1]
+                x_hat = x.reshape(x.shape[-2], col_size)
+                y_hat = self.spdmm(x_hat)
+                return y_hat.reshape(*x.shape[:-2], self.shape[0], x.shape[-1],)
+            elif torch.any(torch.tensor(x.shape) == 1):
+                # If the shapes don't line up for batch multiplication, try to squeeze and try again.
+                return self.__matmul__(torch.squeeze(x))
+            else:
+                # Otherwise, error out.
+                raise RuntimeError(f'invalid tensor found for sparse multiply: mode {dims} tensor found.  Trying to multiply {self.shape} sparse matrix by {x.shape} tensor.')
 
     def to_dense(self):
         '''
