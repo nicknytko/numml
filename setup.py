857315f956cf9cb90622cffc64971423b3c1745f
Nicolas Nytko <nnytko2@illinois.edu>
2025-02-26 14:44:49 -0600

    Add better check for CUDA in setup

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, Extension
+import torch
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension, CppExtension
 import os
 
@@ -15,27 +16,29 @@ def find_cpp_files(directory, allow_cuda):
 # Detect if we have cuda installed and compile accordingly
 native_ext = None
 
-if 'CUDA_HOME' in os.environ or 'CUDA_PATH' in os.environ:
-    print('Detected CUDA, compiling with CUDA acceleration...')
-    native_ext = CUDAExtension(name='numml_torch_cpp',
-                               sources=find_cpp_files('cpp', allow_cuda=True),
-                               include_dirs=[
-                                   os.path.join(os.getcwd(), 'ext/cuCollections/include')
-                               ],
-                               extra_compile_args={
-                                   'nvcc': ['-std=c++17'],
-                                   'cxx': ['-DCUDA_ENABLED=1', '-O2', '-std=c++17', '-w'],
-                               })
+if torch.cuda._is_compiled():
+    print('Detected CUDA install of Torch, compiling with CUDA acceleration...')
+    native_ext = CUDAExtension(
+        name='numml_torch_cpp',
+        sources=find_cpp_files('cpp', allow_cuda=True),
+        include_dirs=[
+            os.path.join(os.getcwd(), 'ext/cuCollections/include')
+        ],
+        extra_compile_args={
+            'nvcc': ['-std=c++17'],
+            'cxx': ['-DCUDA_ENABLED=1', '-O2', '-std=c++17', '-w'],
+        })
 
 else:
-    print('No CUDA detected, compiling CPU implementation only...')
-    native_ext = CppExtension(name='numml_torch_cpp',
-                           sources=find_cpp_files('cpp', allow_cuda=False),
-                           extra_compile_args=[
-                               '-std=c++17',
-                               '-DCUDA_ENABLED=0',
-                               '-O2'
-                           ])
+    print('No CUDA, compiling CPU implementation only...')
+    native_ext = CppExtension(
+        name='numml_torch_cpp',
+        sources=find_cpp_files('cpp', allow_cuda=False),
+        extra_compile_args=[
+            '-std=c++17',
+            '-DCUDA_ENABLED=0',
+            '-O2'
+        ])
 
 setup(ext_modules=[native_ext],
       cmdclass={
